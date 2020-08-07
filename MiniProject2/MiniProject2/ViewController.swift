87ae28ab497574618faafa156ff2523ef95320bf
bkilambi <45800657+bkilambi@users.noreply.github.com>
2020-08-07 10:13:51 -0700

    First draft of Mini Project 2!

---
+++ MiniProject2/MiniProject2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by Apple on 8/6/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
